81ad0da5701b796c560a8d7d573c575033902d07
Anupam Choudhary <87457759+Anupam0-0@users.noreply.github.com>
2024-11-22 01:53:21 +0000

    Added GFG160

---
+++ GFG 160 days Challenge/Day3-ReverseAnArray.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        
+        vector <int> temp;
+        
+        int n = arr.size();
+        temp.resize(n);
+        
+        for(int i = 0 ; i < n; i++){
+            // temp[n-i] = arr[i];
+            temp[i] = arr[n - 1 - i];
+        }
+        
+        arr = temp;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.reverseArray(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
